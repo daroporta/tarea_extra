0aa297e0e01fe68677741ae4f88f421d51015fd1
Darwin Oporta <oportadarwin24@gmail.com>
2023-11-21 21:13:35 -0600

    Multiplicación de elementos de la lista

--- tarea.py
+++ tarea.py
@@ -2,3 +2,18 @@ lista=[10,20,30,40,50,60,70,80,90,100]
 
 
 
+
+
+def multiplicar():
+    multiplica=1
+    for i in lista:
+        multiplica *= i
+    print(f"Resultado de la multiplicación {multiplica}")
+
+multiplicar()
+
+
+
+
+
+
